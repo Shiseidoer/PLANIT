d2fb97177e1ee08467a678fa9f875040f57c5db9
Shiseidoer <119162011+Shiseidoer@users.noreply.github.com>
2020-09-05 22:51:36 +0000

    version 1.7.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,4 +7,26 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - cryptos.tools',
-    titl
+    title: 'cryptos.tools',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: htt
