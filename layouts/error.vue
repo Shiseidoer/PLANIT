a39690eeb1e7e8528e67f8e6e46bc492fca3945f
Shiseidoer <119162011+Shiseidoer@users.noreply.github.com>
2020-09-05 16:44:54 +0000

    version file

--- layouts/error.vue
+++ layouts/error.vue
@@ -21,4 +21,24 @@ export default {
       default: null
     }
   },
-  
+  data () {
+    return {
+      pageNotFound: '404 Not Found',
+      otherError: 'An error occurred'
+    }
+  },
+  head () {
+    const title =
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    return {
+      title
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,10 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - cryptos.tools',
+    titl
