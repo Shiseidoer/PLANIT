355185f11b9f0c5e2d478434fb3005e7b4834cd7
Shiseidoer <119162011+Shiseidoer@users.noreply.github.com>
2023-01-29 14:57:45 +0000

    regulator: core: Release regulator-regulator supplies on error

--- pages/parachains.vue
+++ pages/parachains.vue
@@ -5,3 +5,22 @@
     <v-simple-table dense class="mt-4">
       <template v-slot:default>
         <thead>
+          <tr>
+            <th class="text-left">Names</th>
+            <th class="text-left">Symbol</th>
+            <th class="text-left">Type</th>
+            <th class="text-left">Link</th>
+            <th class="text-left">Rewards</th>
+          </tr>
+        </thead>
+        <tbody class="grey--text mb-2">
+          <tr v-for="item in desserts" :key="item.name">
+            <td>{{ item.name }}</td>
+            <td>{{ item.symbol }}</td>
+            <td>{{ item.type }}</td>
+            <td>{{ item.link }}</td>
+            <td>{{ item.rewards }}</td>
+          </tr>
+        </tbody>
+      </template>
+   
